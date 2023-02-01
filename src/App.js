b59c9d3cf9a6dd3545b4f5f54e768550b3cc3fc2
marufhasan <marufhasansan@gmail.com>
2023-02-01 21:08:11 +0700

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Carousel from "./Pages/Carousel";
 function App() {
   return (
     <div className="App">
       {/* Import Component or Page Here ⬇️ */}
+      <Carousel />
     </div>
   );
 }
