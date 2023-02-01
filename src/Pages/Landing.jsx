7e83c6d7fd595d10d754a1618e4ef9b12312cc93
cartenzaalief <cartenzaalief@gmail.com>
2023-02-01 21:29:21 +0700

    #01-Add Feature Content to Landing

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,80 @@
+const Landing = (props) => {
+    return (
+        <div>
+            {/* FEATURE CONTENT */}
+            <div>
+                <div className="row gap-5 m-5">
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row gap-5 m-5">
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row gap-5 m-5">
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" className="card-img-top mt-2" alt="..." style={{ width: "100%" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <div className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
